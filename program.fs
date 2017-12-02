e2e25992709dacf64b3a60aea130d671fd81ad04
Mateusz Lewko <mateusz.lewko@gmail.com>
2017-12-02 17:51:29 +0100

    Added very simple interpreter.

--- program.fs
+++ program.fs
@@ -1,18 +1,32 @@
 ﻿// open System
+open OpraDB.Interpreter
 open OpraDB.Parser
 open FParsec 
 
 let test p str =
     match run p str with
-    | Success(result, _, _)   -> printfn "Success: %A" result
-    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+    | Success (result, _, _)   -> printfn "Success: %A" result
+    | Failure (errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+let parseAndRun str = 
+    match run parseQuery str with 
+    | Success (result, _, _) -> interpret result 
+    | Failure (errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
 [<EntryPoint>]
 let main argv =
     test (manyWith id "NODES") "NODES (Ba Ab aa)"
     test (manyWith id "NODES") "NODES (s t )"
     test (manyWith pathConstraint "SUCH THAT") "SUCH THAT (s -[pii]-> t x-[p]->y)"
-    test parseQuery "MATCH NODES (s t x y)
-                     SUCH THAT (s -[pii]-> t x-[p]->y )"
 
+    let q1 = "MATCH NODES (s t x y)
+              SUCH THAT (s -[pii]-> t x-[p]->y )"
+
+    test parseQuery q1 
+    parseAndRun q1 
+
+    let q2 = "MATCH NODES (s t x y)
+              SUCH THAT (s -[p1]-> t x-[p2]->y u-[ p3 ]->v)"
+
+    parseAndRun q2
     0 // return an integer exit code    
