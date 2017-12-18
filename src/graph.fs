16ae0f51120c9e8a192912b3a8e741075d60ba01
Mateusz Lewko <mateusz.lewko@gmail.com>
2017-12-18 17:09:15 +0100

    Added basic graph types.

---
+++ src/graph.fs
@@ -0,0 +1,15 @@
+namespace OpraDB
+
+module Graph = 
+
+    type Edge = int * int 
+    type PropertyValue = IntVal of int | StringVal of int 
+    type Properties = Map<string, PropertyValue>
+    type EdgeData = Map<Edge, Properties>
+
+    type NodeMap = Map<int, Node> 
+    and Node = {
+            id         : int 
+            data       : Properties
+            neighbours : NodeMap
+        }
