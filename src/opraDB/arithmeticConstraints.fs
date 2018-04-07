bd6c659b24a30bb4a5d8eb920764f12a903ee3b6
Mateusz Lewko <mateusz.lewko@gmail.com>
2018-04-07 02:08:30 +0200

    Clean-up in existsSolutionInner.

--- src/opraDB/arithmeticConstraints.fs
+++ src/opraDB/arithmeticConstraints.fs
@@ -72,35 +72,17 @@ module ArithmeticConstraints =
         let curr = List.map (fun a -> a, 0) attrs |> Map.ofList
         List.fold (addNodeAttributes graph) curr cycle
 
-    // let constraintToInequality (solver : Solver)
-    //                            (ArithmeticConstraint (l, op, r)) = 
-    //     solver.Add ()
-
-    type AttrDelta = { path : Identifier; attr: Identifier; delta: int }
-
-    let mutable cnt = 0
-
     let private existsSolutionInner constraints cyclesDeltas ctx =
-        
-        // System.GC.KeepAlive (ctx2)
-        // Native.Z3_del_context Gs.globalCtx
-        // Gs.globalCtx <- ctx2
-        // System.GC.KeepAlive (Gs)
-        use solver : Solver =  Solver.create ctx
-        // System.GC.KeepAlive (solver)
-        // solver.Push()
-        cnt <- cnt + 1
-
+        use solver   = Solver.create ctx
         let cycleCnt = List.length cyclesDeltas
-        // let ctx =context
+
         /// Value of alpha-i represents  how many times to traverse i-th cycle
-        let alphas   = Arr.init cycleCnt (fun i -> (ctx.MkIntConst (sprintf "%d-alpha-%d" cnt i)))
+        let alphas   = Arr.init cycleCnt (fun i -> (ctx.MkIntConst (sprintf "alpha-%d" i)))
         /// All mappings from (path, attribute) to delta for a given cycle
         let deltas   = Array.ofList cyclesDeltas
 
-        // printfn "alphas: %A" alphas
-        // printfn "deltas: %A" deltas
-        
+        let mkConst (i : int) = ctx.MkAdd (ctx.MkInt 0, ctx.MkInt i)
+
         /// Mapping from (path, attribute) to Int expression representing 
         /// sum of alpha_i * delta_i for a given (path, attribute)
         let attrAlphas = 
@@ -112,51 +94,33 @@ module ArithmeticConstraints =
                 let indexes = Array.ofSeq indexes
                 let getAlpha i = 
                     let delta : int = Map.find id deltas.[i]
-                    
-                    System.Console.WriteLine (sprintf "i: %d" i)
-                    System.Console.Out.Flush ()
-                    System.Console.WriteLine (sprintf "a: %O" alphas.[i])
-                    System.Console.Out.Flush ()
-                    System.Console.WriteLine (sprintf "d: %d" delta)
-                    System.Console.Out.Flush ()
-
                     ctx.MkMul (alphas.[i], ctx.MkInt delta)
 
-                let rec fold = 
-                    function 
-                    | []    -> ctx.MkAdd (ctx.MkInt 0, ctx.MkInt 0)
-                    | [x]   -> x
-                    | x::y::xs -> fold (ctx.MkAdd(x, y)::xs)
+                let add l r = ctx.MkAdd (l, r)
 
                 Array.map getAlpha indexes
-                |> Array.toList
-                |> fold
+                |> Array.fold add (mkConst 0)
 
             Array.indexed deltas 
             |> Array.fold attrIndexes Map.empty
             |> Map.map constructExpr 
 
-        // let (>>*) x f = f x; x
-
         let rec evalOperand =
             function 
-            | IntALiteral i -> ctx.MkAdd (ctx.MkInt 0, ctx.MkInt i)
+            | IntALiteral i -> mkConst i
             | Add (l, r)    -> ctx.MkAdd (evalOperand l, evalOperand r)
             | Mult (l, r)   -> ctx.MkMul (evalOperand l, evalOperand r)
-            | SumBy (p, l)  -> match Map.tryFind (p, l) attrAlphas with 
-                               | Some x -> x
-                               | None   -> ctx.MkAdd (ctx.MkInt 0, ctx.MkInt 0)
-                            //    |> Option.defaultValue (IntVal 0I) 
-                            //    >>*  (printfn "op: %O")
+            | SumBy (p, l)  -> Map.tryFind (p, l) attrAlphas 
+                               |> Option.defaultValue (mkConst 0) 
 
         let evalOperator lhs rhs = 
             function 
-            | Eq  -> ctx.MkEq (lhs, rhs)
+            | Eq  -> ctx.MkEq       (lhs, rhs)
             | Neq -> ctx.MkDistinct (lhs, rhs)
-            | Leq -> ctx.MkLe (lhs, rhs)
-            | Geq -> ctx.MkGe (lhs, rhs)
-            | Ge  -> ctx.MkGt (lhs, rhs)
-            | Le  -> ctx.MkLt (lhs, rhs)
+            | Leq -> ctx.MkLe       (lhs, rhs)
+            | Geq -> ctx.MkGe       (lhs, rhs)
+            | Ge  -> ctx.MkGt       (lhs, rhs)
+            | Le  -> ctx.MkLt       (lhs, rhs)
 
         let evalConstraint (ArithmeticConstraint (lhs, op, rhs)) =
             evalOperator (evalOperand lhs) (evalOperand rhs) op
@@ -164,46 +128,22 @@ module ArithmeticConstraints =
         let constraintsExpr = Array.ofList constraints 
                               |> Array.map evalConstraint 
 
-        // let s = Solver ()
-        // let ctx = (Gs.context())
-        // use ctx = new Context ()
-        // // Gs.context
-        // let a = 
-
         let result : SolveResult = 
             let exprs = Array.map (fun a -> ctx.MkGe (a, ctx.MkInt 0)) alphas
                         |> Array.append constraintsExpr
-                        // |> Array.map (function BoolExpr e -> e)
 
             solver.Add exprs
             Solver.check solver
-            // Z3.Solve (And exprs)
-            
-            // Z3.Solve (And constraintsExpr)
-        // true
-        // printfn "result %O" result
-        // let g = (Gs.context())
-        // let solver = g.MkSimpleSolver ()
-        // solver.Push ()
-        // let s = solver.Check () 
-        // let ss = Solver.check solver
-        // solver.p
+
         match result with 
         | Solution s -> 
             printfn "%A" s
             true 
         | _          -> false
 
-
     let existsSolution constraints cyclesDeltas =
         use ctx = Context.create ()
-        // Gs.globalCtx <- Some ctx
-        let res = existsSolutionInner constraints cyclesDeltas ctx
-        // Gs.globalCtx <- None
-        // System.GC.Collect ()
-        // ctx.Dispose ()
-        res
-
+        existsSolutionInner constraints cyclesDeltas ctx
 
     let inequalitiesSatisfied mKEdges predecessors attrs graph constraints = 
         let subGraph, visited = Graph.Utils.restoreGraph predecessors mKEdges
